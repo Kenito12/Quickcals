193ab06cac64ff564630184a2b4d7bc4584f6bbf
Ken <nontsuke.ken@gmail.com>
2024-10-03 00:13:24 -0400

    progress bar js added, wave svg added in the bg

--- script.js
+++ script.js
@@ -14,12 +14,10 @@ startBut.onclick = () => {
     setTimeout(() => {
         startScreen.style.display = "none"
         resultScreen.style.display = "block"
+        bar.animate(1.0); 
+        bar2.animate(100/100); 
     }, 300)
 }
-
-
-
-
 
 
 // option use to setup fetch
@@ -31,3 +29,65 @@ fetch('https://api.edamam.com/api/nutrition-data?app_id=ee662e01&app_key=5410242
 .then(apiResult => {
     console.log(apiResult)})
 .catch(err => console.error(err));
+
+
+
+
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+var bar = new ProgressBar.Circle(calMeter, {
+    color: '#003069',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 8,
+    trailWidth: 8,
+    easing: 'easeInOut',
+    duration: 2000,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#33D6EC', width: 8 },
+    to: { color: '#33D6EC', width: 8 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 702);
+        if (value === 0) {
+        circle.setText('');
+    } else {
+        circle.setText(value + `<br><span class="meterSub">Calories</span>`);
+    }
+    }
+    });
+
+
+    var bar2 = new ProgressBar.Circle(proteinMeter, {
+    color: '#003069',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 8,
+    trailWidth: 8,
+    easing: 'easeInOut',
+    duration: 2000,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#ECA233', width: 8 },
+    to: { color: '#ECA233', width: 8 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+      var value = Math.round(circle.value() * 102.9);
+        if (value === 0) {
+        circle.setText('kcal');
+        } else {
+        circle.setText(value + `<br><span class="meterSub">Grams</span>`);
+        }
+
+    }
+    })
